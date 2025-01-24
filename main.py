1b487a4583cc0d50bbe1853a78db5fc84d0e87b1
Angel Guevara Ros <aguevara@d4dom.net>
2025-01-24 14:32:34 +0100

    add query to analyze the sites already processed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cloud_tasks import launch_task
+from google.cloud import bigquery
 
 import os
 from flask import Flask
@@ -11,38 +12,36 @@ queue_name_analyze = os.environ.get('queue_name_analyze')
 queue_region = os.environ.get('queue_region')
 service_account = os.environ.get('service_account')
 
+# bigquery client
+client_bigquery = bigquery.Client()
+
+
 @app.post("/")
 def analyze_sharepoint():
   
   sharepoint_application_name = 'TRD077-BuscadorCognitivo-GCP-spo-api4'
   process_queue = 'process-sharepoint-4'
 
-  sites_list =[
-    {
-      "site_name": "IstanbulOffice",
-      "site_id_short": "IstanbulOffice"
-    },
-    {
-      "site_name": "",
-      "site_id_short": "TR-SIM"
-    },
-    {
-      "site_name": "Buscador Cognitivo",
-      "site_id_short": "BuscadorCognitivo"
-    },
-    {
-      "site_name": "setupCognitiveSearch",
-      "site_id_short": "setupCognitiveSearch"
-    },
-    {
-      "site_name": "Biblioteca",
-      "site_id_short": "Biblioteca"
-    },
-    {
-      "site_name": "FTP_8595ASEVASA",
-      "site_id_short": "FTP_8595ASEVASA"
-    }
-  ]
+  # check sites already proccessed for new files
+  query = """
+          SELECT DISTINCT SD.site_name, SD.site_web_url 
+          FROM `rg-trd077-pro.configuration_details.stats_all_detail_summary` AS S JOIN `rg-trd077-pro.configuration_details.sites_and_drives` AS SD ON S.site_name=SD.site_name 
+        """
+
+  try:
+    query_job = client_bigquery.query(query)
+    query_result = query_job.result()
+  except:
+    print("ERROR IN SQL QUERY")
+    return "ERROR IN SQL QUERY", 200
+  
+  # get query results in the correct format
+  rows = [dict(row) for row in query_result]
+  sites_list=[]
+  for i in rows:
+    sites_list.append({'site_name':i['site_name'], 'site_id_short': i['site_web_url'].split("/")[-1]})
+
+
 
   for site in sites_list:
     site['sharepoint_application_name'] = sharepoint_application_name
